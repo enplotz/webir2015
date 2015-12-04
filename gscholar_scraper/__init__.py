15e713211e4610500882265e33a46b38177fe3df
Manuel Hotz <manuel.hotz@enplotz.de>
2015-12-04 14:45:22 +0100

    Don't loop the init script

--- gscholar_scraper/__init__.py
+++ gscholar_scraper/__init__.py
@@ -1,9 +1,6 @@
 import scrapy.cmdline as cmdline
 import time
 
-while True:
-    print "NEW RUN"
-    cmdline.execute('scrapy crawl authorLabels -o popular_names.csv -t csv'.split(' '))
 
-    print "executed "
-    time.sleep(35)
+cmdline.execute('scrapy crawl authorLabels -s JOBDIR=crawls/author_labels -o popular_names.csv -t csv'.split(' '))
+
