f4984bd3726690109f4f577c4990c6b38551037f
Manuel Hotz <manuel.hotz@enplotz.de>
2015-12-04 14:46:30 +0100

    Default update_time for GScholarItem

--- gscholar_scraper/gscholar_scraper/pipelines.py
+++ gscholar_scraper/gscholar_scraper/pipelines.py
@@ -5,6 +5,14 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import items
+from time import ctime
+
+# we set the default values in this pipeline step
+class DefaultValuesForItem(object):
+    def process_item(self, item, spider):
+        if isinstance(item, items.GScholarItem):
+            item.setdefault('updated_at', ctime())
+        return item
 
 class GscholarScraperPipeline(object):
     def process_item(self, item, spider):
