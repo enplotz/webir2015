30c25bb98d8b32ce528a033ed382aa3c4917975e
Manuel Hotz <manuel.hotz@enplotz.de>
2015-12-09 14:24:42 +0100

    Log level configurable

--- gscholar_scraper/main.py
+++ gscholar_scraper/main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     parser.add_argument('--job_dir','-s', type=str, help='job directory if execution should save its state to be resumable')
     parser.add_argument('--output_filename','-o', type=str, help='filename for export')
     parser.add_argument('--output_filetype','-t', type=str, help='file type for export')
+    parser.add_argument('--log_level', '-L', type=str, help='log level')
 
     parser.add_argument('spider', metavar='-c', type=str, help='name of the spider to run')
     # cmdline.execute('scrapy crawl authorLabels -s JOBDIR=crawls/author_labels -o popular_names.csv -t csv'.split(' '))
@@ -19,4 +20,6 @@ if __name__ == '__main__':
         command += ' -t %s' % args.output_filename
     if args.output_filetype:
         command += ' -o %s' % args.output_filetype
+    if args.log_level:
+        command += ' -L %s' % args.log_level
     cmdline.execute(command.split(' '))
