f19e366aecc7c10467e8c72b6b6a06cc3e6045ec
Manuel Hotz <manuel.hotz@enplotz.de>
2015-12-10 17:49:07 +0100

    Compare researcher with avg field researcher

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,17 +6,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, String
 from sqlalchemy.sql.expression import cast
 from sqlalchemy.dialects import postgresql
-
 from flask_bootstrap import Bootstrap
-
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
 from markupsafe import escape
-
 from os.path import join, dirname
 from dotenv import load_dotenv
 from os import environ
-
 from flask_debug import Debug
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -41,6 +37,23 @@ Label = Base.classes.labels
 toolbar = DebugToolbarExtension(app)
 
 
+def avg_cite_sql(fields):
+    sql = 'WITH ' + \
+          'sub_authors AS ( ' + \
+          'SELECT * FROM authors WHERE fields_of_study @> ARRAY[' + \
+          ', '.join(map(lambda f: '\'{}\''.format(f), fields)) + \
+          ']::varchar[] AND cited IS NOT NULL AND measures IS NOT NULL' + \
+          '  ),' + \
+          '    unnested AS (' + \
+          '      SELECT id, generate_series(1, 6), unnest(measures) FROM sub_authors),' + \
+          '    t1 AS (' + \
+          '      SELECT generate_series, avg(unnest) AS average, NULL as avg_cited FROM unnested GROUP BY generate_series ORDER BY generate_series),' + \
+          '  t2 as (SELECT NULL::numeric[] as array_agg , avg(cited) as avg_cited FROM sub_authors GROUP BY sub_authors.fields_of_study)' + \
+          'SELECT array_agg(average) as avg_measures, min(t2.avg_cited) as avg_cited FROM t1, t2;'
+    # print(sql)
+    return sql
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     errors = []
@@ -52,12 +65,13 @@ def index():
 def field_term(search_term):
     return '_'.join(search_term.lower().split(' '))
 
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     errors = []
     if request.method == 'POST':
         search_term = request.form['entity_value']
-        print(search_term)
+        # print(search_term)
         fos_search_term = field_term(search_term)
         try:
             session = Session(engine)
@@ -68,11 +82,13 @@ def search():
                 fields = session.query(Label).filter(Label.field_name.like('%' + fos_search_term + '%')).all()
             finally:
                 session.close()
-            return render_template('search_result.html', researchers=res, search_term = search_term, fos_search_term = fos_search_term, fields=fields)
+            return render_template('search_result.html', researchers=res, search_term=search_term,
+                                   fos_search_term=fos_search_term, fields=fields)
         except Exception as e:
             errors.append(e)
 
     return render_template('search_entity.html', entity_name='Researcher or Field', action='/search', errors=errors)
+
 
 @app.route('/researcher', methods=['GET'])
 def find_researcher():
@@ -81,6 +97,7 @@ def find_researcher():
     #     researcher = request.form['entity_value']
     #     return redirect(url_for('show_researcher_profile', id=researcher))
     # return show_search_form('Researcher', '/researcher')
+
 
 @app.route('/researcher/<id>')
 def show_researcher_profile(id):
@@ -100,12 +117,29 @@ def show_researcher_profile(id):
 
     return render_template('researcher.html', researcher=res, publications=pub)
 
+
 @app.route('/researcher/<id>/fos/<field_name>')
 def compare_researcher_fos(id, field_name):
-    pass
+    errors = []
+    results = {}
+    try:
+        session = Session(engine)
+        try:
+            res = session.query(Author).get(id)
+            avg = session.execute(avg_cite_sql([field_name])).fetchone()
+            # print(avg)
+        finally:
+            session.close()
+    except Exception as e:
+        errors.append(e)
+        return render_template('compare.html', errors=errors)
+
+    return render_template('compare.html', researcher=res, avg=avg, field_name=field_name)
+
 
 def show_search_form(entity_name, action):
     return render_template('search_entity.html', entity_name=entity_name, action=action)
+
 
 @app.route('/fos', methods=['POST', 'GET'])
 def find_field():
@@ -115,6 +149,7 @@ def find_field():
     # return show_search_form('Field Of Study', '/fos')
     return redirect(url_for('search'), 302)
 
+
 @app.route('/fos/<field_name>')
 def show_field(field_name):
     errors = []
@@ -123,16 +158,18 @@ def show_field(field_name):
         # QUERY FOR AUTHORS HAVING THE FOS
         session = Session(engine)
         try:
-            res = session.query(Author).filter(Author.fields_of_study.contains(cast([field_name], postgresql.ARRAY(String)))).all()
+            res = session.query(Author).filter(
+                Author.fields_of_study.contains(cast([field_name], postgresql.ARRAY(String)))).all()
         finally:
             session.close()
     except Exception as e:
         errors.append(e)
         return render_template('fos.html', errors=errors)
 
-    print('Got {0} results.'.format(len(res)))
+    # print('Got {0} results.'.format(len(res)))
     return render_template('fos.html', results=res, field_of_study=field_name)
 
+
 if __name__ == '__main__':
-    print(environ['APP_SETTINGS'])
+    # print(environ['APP_SETTINGS'])
     app.run(host='0.0.0.0', port=5000)
