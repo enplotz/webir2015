2ddc67a86ba458c1d794742b8ae4dba74f649b99
Manuel Hotz <manuel.hotz@enplotz.de>
2015-12-10 12:09:05 +0100

    First experimental chart support

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,11 +8,9 @@ from sqlalchemy.sql.expression import cast
 from sqlalchemy.dialects import postgresql
 
 from flask_bootstrap import Bootstrap
-from flask_nav import Nav
+
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
-from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
-from flask_nav import register_renderer
 from markupsafe import escape
 
 from os.path import join, dirname
@@ -39,29 +37,7 @@ Base.prepare(engine, reflect=True)
 Author = Base.classes.authors
 
 toolbar = DebugToolbarExtension(app)
-nav = Nav()
 
-from flask_bootstrap.nav import BootstrapRenderer
-
-class CustomRenderer(BootstrapRenderer):
-    def visit_Navbar(self, node):
-        nav_tag = super(CustomRenderer, self).visit_Navbar(node)
-        nav_tag['class'] += 'navbar navbar-inverse navbar-fixed-top'
-        return nav_tag
-
-register_renderer(app, 'custom', CustomRenderer)
-
-# We're adding a navbar as well through flask-navbar. In our example, the
-# navbar has an usual amount of Link-Elements, more commonly you will have a
-# lot more View instances.
-nav.register_element('frontend_top', Navbar(
-    View('GScholar Analytics', '.index'),
-    View('Analyse', '.index'),
-    # View('Forms Example', '.example_form'),
-    View('Debug-Info', 'debug.debug_root'),
-))
-
-nav.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
