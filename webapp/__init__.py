44f7ccd80e9620dd5440460fe03d945949fdc9b7
Manuel Hotz <manuel.hotz@enplotz.de>
2015-12-14 11:49:45 +0100

    Global session; DT show queries; Cache metrics on index page

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,29 +1,31 @@
-from flask import Flask, render_template, request
+from os import environ
+from os.path import join, dirname
+
+from dotenv import load_dotenv
+from flask import Blueprint, render_template, redirect, url_for,jsonify, request
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_debug import Debug
 from flask_debugtoolbar import DebugToolbarExtension
-from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, String
+from sqlalchemy import func
+from sqlalchemy.dialects import postgresql
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, String
 from sqlalchemy.sql.expression import cast
-from sqlalchemy.dialects import postgresql
-from flask_bootstrap import Bootstrap
-from flask import Blueprint, render_template, flash, redirect, url_for,jsonify, request
-from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
-from markupsafe import escape
-from os.path import join, dirname
-from dotenv import load_dotenv
-from os import environ
-from flask_debug import Debug
-from queries.dashboard import MOST_CITED_BY_FIELD, avg_cite_sql,top_authors_m, time_series
+from flask.ext.cache import Cache
+
+from queries.dashboard import avg_cite_sql,top_authors_m, time_series
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 app.config.from_object(environ['APP_SETTINGS'])
-
 Bootstrap(app)
 Debug(app)
+# Check Configuring Flask-Cache section for more details
+cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 
 frontend = Blueprint('frontend', __name__)
 app.register_blueprint(frontend)
@@ -31,11 +33,31 @@ app.register_blueprint(frontend)
 Base = automap_base()
 engine = create_engine(app.config['SQL_ALCHEMY_DATABASE_URI'])
 Base.prepare(engine, reflect=True)
+session = Session(engine)
+
+# TODO make base classes use the correct engine
+# Currently, this uses sqlite o.O: `num_authors = Author.query.count()`
 Author = Base.classes.authors
 Document = Base.classes.documents
 Label = Base.classes.labels
 
 toolbar = DebugToolbarExtension(app)
+
+# Enable SQLAlchemy in Debug Toolbar without flask-sqlalchemy :)
+from flask.ext.sqlalchemy import _EngineDebuggingSignalEvents
+_EngineDebuggingSignalEvents(engine, app.import_name).register()
+
+
+# Cache for x seconds
+@cache.cached(timeout=60)
+def get_metrics():
+    return {
+        'num_authors': session.query(Author).count(),
+        'num_authors_fully': session.query(Author).filter(Author.measures.isnot(None)).count(),
+        'num_documents': session.query(Document).count(),
+        'num_fields': session.query(Label).count(),
+    }
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -43,24 +65,25 @@ def index():
     ranks = []
     series_docs = []
     series_cites = []
+    metrics = None
 
     # Dashboard
     # Top-Researcher by Field (cited)
     # Max-Cites by Field
-    # Avg # Cites by Field
+    # Avg # Cites by
     try:
-        session = Session(engine)
-        try:
-            ranks = session.execute(top_authors_m(1,10))
-            series_docs = session.execute(time_series(False))
-            series_cites = session.execute(time_series(True))
+        ranks = session.execute(top_authors_m(1,10))
+        series_docs = session.execute(time_series(False))
+        series_cites = session.execute(time_series(True))
+        metrics = get_metrics()
+        num_authors = session.query(func.count(Author.id)).scalar()
+        print("Num authors %d " % num_authors)
 
-        finally:
-            session.close()
     except Exception as e:
         errors.append(e)
-        return render_template('index.html', errors=errors,rankings=ranks)
-    return render_template('index.html', rankings= ranks, errors= errors)
+        return render_template('index.html', errors=errors, rankings=ranks, metrics=None)
+
+    return render_template('index.html', errors=errors, rankings=ranks, metrics=metrics, num_authors=num_authors)
 
 
 def field_term(search_term):
@@ -71,11 +94,7 @@ def getRankings(m):
     errors = []
     result = []
     try:
-        session = Session(engine)
-        try:
-            result = session.execute(top_authors_m(int(m),10))
-        finally:
-            session.close()
+        result = session.execute(top_authors_m(int(m),10))
     except Exception as e:
         errors.append(e)
     return jsonify(results = [dict(row) for row in result], errors =errors)
@@ -85,13 +104,9 @@ def getTimeSeries():
     errors = []
     results= []
     try:
-        session = Session(engine)
-        try:
-            docCounts = session.execute(time_series(False))
-            citeCounts = session.execute(time_series(True))
-            results = [[dict(row) for row in docCounts], [dict(row) for row in citeCounts]]
-        finally:
-            session.close()
+        docCounts = session.execute(time_series(False))
+        citeCounts = session.execute(time_series(True))
+        results = [[dict(row) for row in docCounts], [dict(row) for row in citeCounts]]
     except Exception as e:
         errors.append(e)
     return jsonify(results = results, errors =errors)
@@ -105,30 +120,21 @@ def search():
         search_term = request.form['entity_value']
         # print(search_term)
         fos_search_term = field_term(search_term)
+        res = None
+        fields = None
         try:
-            session = Session(engine)
-            res = None
-            fields = None
-            try:
-                res = session.query(Author).filter(Author.name.like('%' + search_term + '%')).all()
-                fields = session.query(Label).filter(Label.field_name.like('%' + fos_search_term + '%')).all()
-            finally:
-                session.close()
+            res = session.query(Author).filter(Author.name.like('%' + search_term + '%')).all()
+            fields = session.query(Label).filter(Label.field_name.like('%' + fos_search_term + '%')).all()
             return render_template('search_result.html', researchers=res, search_term=search_term,
-                                   fos_search_term=fos_search_term, fields=fields)
+                               fos_search_term=fos_search_term, fields=fields)
         except Exception as e:
             errors.append(e)
-
     return render_template('search_entity.html', entity_name='Researcher or Field', action='/search', errors=errors)
 
 
 @app.route('/researcher', methods=['GET'])
 def find_researcher():
     return redirect(url_for('search'), 302)
-    # if request.method == 'POST':
-    #     researcher = request.form['entity_value']
-    #     return redirect(url_for('show_researcher_profile', id=researcher))
-    # return show_search_form('Researcher', '/researcher')
 
 
 @app.route('/researcher/<id>')
@@ -136,13 +142,8 @@ def show_researcher_profile(id):
     errors = []
     results = {}
     try:
-        # QUERY FOR AUTHORS HAVING THE FOS
-        session = Session(engine)
-        try:
-            res = session.query(Author).get(id)
-            pub = session.query(Document).filter(Document.author_id == id).all()
-        finally:
-            session.close()
+        res = session.query(Author).get(id)
+        pub = session.query(Document).filter(Document.author_id == id).all()
     except Exception as e:
         errors.append(e)
         return render_template('researcher.html', errors=errors)
@@ -155,13 +156,9 @@ def compare_researcher_fos(id, field_name):
     errors = []
     results = {}
     try:
-        session = Session(engine)
-        try:
-            res = session.query(Author).get(id)
-            avg = session.execute(avg_cite_sql([field_name])).fetchone()
-            # print(avg)
-        finally:
-            session.close()
+        res = session.query(Author).get(id)
+        avg = session.execute(avg_cite_sql([field_name])).fetchone()
+        # print(avg)
     except Exception as e:
         errors.append(e)
         return render_template('compare.html', errors=errors)
@@ -175,10 +172,6 @@ def show_search_form(entity_name, action):
 
 @app.route('/fos', methods=['POST', 'GET'])
 def find_field():
-    # if request.method == 'POST':
-    #     fos = field_term(request.form['entity_value'])
-    #     return redirect(url_for('show_field', field_name=fos))
-    # return show_search_form('Field Of Study', '/fos')
     return redirect(url_for('search'), 302)
 
 
@@ -187,18 +180,11 @@ def show_field(field_name):
     errors = []
     results = {}
     try:
-        # QUERY FOR AUTHORS HAVING THE FOS
-        session = Session(engine)
-        try:
-            res = session.query(Author).filter(
-                Author.fields_of_study.contains(cast([field_name], postgresql.ARRAY(String)))).all()
-        finally:
-            session.close()
+        res = session.query(Author).filter(
+            Author.fields_of_study.contains(cast([field_name], postgresql.ARRAY(String)))).all()
     except Exception as e:
         errors.append(e)
         return render_template('fos.html', errors=errors)
-
-    # print('Got {0} results.'.format(len(res)))
     return render_template('fos.html', results=res, field_of_study=field_name)
 
 
