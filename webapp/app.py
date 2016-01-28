4657b838d1f937958d3e64c4c0468e58d05f4aec
Manuel Hotz <manuel.hotz@enplotz.de>
2016-01-28 22:02:40 +0100

    Implement Multi-Search
    - A list of author links can now be submitted
    - The list is then crawled and can later be analyzed a bit

--- webapp/app.py
+++ webapp/app.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from os import environ
 
 import requests
+from datatables import ColumnDT, DataTables
 from flask import Blueprint, render_template, redirect, url_for, request, Response
 from flask import Flask
 from flask import jsonify, abort
@@ -17,9 +18,13 @@ from sqlalchemy.dialects import postgresql
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.expression import cast
+from urlparse import parse_qs, urlparse
+import json
+import grequests
 
 import queries.co_network as co
 from queries.dashboard import avg_measures_sql,top_authors_m, time_series_documents, time_series_cited
+import queries.compare as cmp
 
 # dotenv_path = join(dirname(__file__), '.env')
 # load_dotenv(dotenv_path)
@@ -123,10 +128,6 @@ def index():
     series_cites = []
     metrics = None
 
-    # Dashboard
-    # Top-Researcher by Field (cited)
-    # Max-Cites by Field
-    # Avg # Cites by
     try:
         r = rankings()
         ranks = r['ranks']
@@ -139,23 +140,75 @@ def index():
         return render_template('pages/index.html', errors=errors, rankings=ranks, metrics=None)
     return render_template('pages/index.html', errors=errors, rankings=ranks, metrics=metrics, num_authors=metrics['num_authors'])
 
+def extract_links(text):
+    """Extracts links from a list of links divided by new lines
+    """
+    # \r\n
+    # Split by newline, linefeeds will be stripped with other whitespace
+    lines = (l for l in [line.strip() for line in text.split('\n')] if l)
+    users = (parse_qs(urlparse(line).query).get('user')[0] for line in lines)
+    return list(users)
 
-@app.route('/compare', methods=['POST', 'GET'])
+def request_for_author(author_id):
+    'project=gscholar_scraper&spider=author_complete&start_authors={}'
+    endpoint = 'http://{0}:{1}/schedule.json'.format(environ.get('SCRAPYD_HOST'), environ.get('SCRAPYD_PORT'))
+    params = {'project': 'gscholar_scraper', 'spider' : 'author_complete', 'start_authors' : author_id}
+    print params
+    return grequests.post(endpoint, data=params)
+
+@app.route('/multisearch', methods=['POST', 'GET'])
+def multi_search():
+    # POST is used for starting the search/crawling
+    # GET will display the form
+    # GET is also used for displaying the results (for sharable links)
+    researchers = None
+    ids = None
+    if request.method == 'POST':
+        ids = extract_links(request.form['links'])
+        # TODO schedule jobs and return job_ids for checking on page
+
+        rs = (request_for_author(i) for i in ids)
+        responses = grequests.map(rs)
+
+        # Response object will be something like:
+        # {
+        #   "status": "ok",
+        #   "jobid": "60cd913ac28e11e59a4d0242ac11000d",
+        #   "node_name": "58394e5efede"
+        # }
+
+        jobs = [json.loads(r.text) for r in responses]
+        for idx, author_id in enumerate(ids):
+            jobs[idx]['id'] = author_id
+
+        return render_template('pages/multi/progress.html', header='Multi-Search Results', num=len(ids), ids=ids,
+                               jobs=jobs)
+    elif request.method == 'GET':
+        return render_template('pages/multi/search.html', header='Multi-Search')
+
+@app.route('/compare', methods=['GET'])
 def compare_authors():
     # This route should enable the user to compare n authors.
     # We have to start a crawling job
     # wait until it is finished
     # redirect to a new/the same page and display the results
-    pass
+    author_ids = request.args.getlist('author_id')
+    errors = []
+    print author_ids
+    try:
+        researchers = session.query(Author).filter(Author.id.in_(author_ids)).all()
+        authors_cited_per_year_query = cmp.authors_cited_per_year(author_ids)
+        # We build our data structure how metricsgraphics.js does expect it
+        data = [{'id': a.id, 'name' : a.name, 'fos': a.fields_of_study if a.fields_of_study else [],
+                 'cite_counts' : [ {'year': t[1], 'count': t[0]} for t in zip(a.cite_count, a.years)],
+                 'pub_counts' : [ {'year': t[1], 'count': t[0]} for t in zip(a.pub_count, a.years)]
+                 } for a in session.execute(authors_cited_per_year_query)]
 
+        return render_template('pages/multi/results.html', header='Multi-Search Results', researchers=researchers, metrics=data)
+    except Exception as e:
+        errors.append(e)
+        return render_template('pages/multi/results.html', header='Multi-Search Results', errors=errors, researchers=None, metrics=None)
 
-@app.route('/yield')
-def yield_test():
-    def inner():
-        for x in range(100):
-            time.sleep(1)
-            yield '%s<br/>\n' % x
-    return Response(inner(), mimetype='text/html')
 
 def field_term(search_term):
     return '_'.join(search_term.lower().split(' '))
@@ -177,13 +230,14 @@ def getTimeSeries():
     errors = []
     results= []
     try:
-        docCounts = session.execute(time_series(False))
-        citeCounts = session.execute(time_series(True))
+        ts_docs_q = time_series_documents()
+        ts_cites = time_series_cited()
+        docCounts = session.execute(ts_docs_q).scalar(),
+        citeCounts = session.execute(ts_cites).scalar()
         results = [[dict(row) for row in docCounts], [dict(row) for row in citeCounts]]
     except Exception as e:
         errors.append(e)
     return jsonify(results = results, errors =errors)
-
 
 
 @app.route('/search', methods=['GET', 'POST'])
@@ -204,18 +258,41 @@ def search():
                                    )
         except Exception as e:
             errors.append(e)
-    return render_template('pages/search/entity.html', entity_name='Researcher or Field', action='/search', errors=errors)
+    return render_template('pages/search/entity.html', entity_name='Researcher or Field', action='/search', errors=errors, header='Search')
+
+
+def none_to_empty_str(data):
+    if not data:
+        return None
+    else:
+        return data
+
+# The main listings of the models are handled by DataTables
+# We use DT's ajax calls and server-side processing.
+# So we need one route for the html page and one route that returns json
+# for the data access.
 
 
 @app.route('/researchers', methods=['GET'])
 def researchers():
-    errors = []
-    try:
-        res = session.query(Author).all()
-        return render_template('pages/researchers.html', researchers=res)
-    except Exception as e:
-        errors.append(e)
-    return render_template('pages/researchers.html', errors=errors)
+    return render_template('pages/researchers.html')
+
+
+@app.route('/api/researchers', methods=['GET'])
+def api_researchers():
+    columns = [ColumnDT('id'),
+               ColumnDT('name'),
+               ColumnDT('fields_of_study'),
+               ColumnDT('cited', filter=none_to_empty_str), \
+               ColumnDT('measures', filter=none_to_empty_str),
+               ColumnDT('org', filter=none_to_empty_str),
+               ColumnDT('hasCo', filter=none_to_empty_str),
+               ColumnDT('created_at', filter=str),
+               ColumnDT('updated_at', filter=str),
+               ]
+    query = session.query(Author)
+    row_table = DataTables(request.args, Author, query, columns)
+    return jsonify(**row_table.output_result())
 
 
 @app.route('/researcher', methods=['GET'])
@@ -261,14 +338,16 @@ def compare_researcher_fos(id, field_name):
 
 @app.route('/fields', methods=['GET'])
 def fields():
-    errors = []
-    try:
-        res = session.query(Label).all()
-        return render_template('pages/fields.html', fields=res)
-    except Exception as e:
-        errors.append(e)
-        print e
-    return render_template('pages/fields.html', errors=errors)
+    return render_template('pages/fields.html')
+
+@app.route('/api/fields', methods=['GET'])
+def api_fields():
+    columns = [ColumnDT('field_name'),
+               ColumnDT('created_at', filter=str),
+               ColumnDT('updated_at', filter=str), ]
+    query = session.query(Label)
+    row_table = DataTables(request.args, Label, query, columns)
+    return jsonify(**row_table.output_result())
 
 
 @app.route('/fos', methods=['POST', 'GET'])
@@ -291,14 +370,34 @@ def show_field(field_name):
 
 @app.route('/documents', methods=['GET'])
 def documents():
-    errors = []
-    try:
-        res = session.query(Document).all()
-        return render_template('pages/documents.html', documents=res)
-    except Exception as e:
-        errors.append(e)
-        print e
-    return render_template('pages/documents.html', errors=errors)
+    return render_template('pages/documents.html')
+
+
+@app.route('/api/documents', methods=['GET'])
+def api_documents():
+    columns = [ColumnDT('id'),
+               ColumnDT('author_id'),
+               ColumnDT('title'),
+               ColumnDT('cite_count', filter=none_to_empty_str),
+               ColumnDT('year', filter=none_to_empty_str),
+               ColumnDT('created_at', filter=str),
+               ColumnDT('updated_at', filter=str),
+               ]
+
+    # Currently we filter out documents that have no publication date or no citations.
+    # This has two reasons: 1) documents without publication dates are not really interesting and
+    # often times not real publications
+    # 2) there exists no option to set SQL order by null behavior in the currently used libraries
+    # (sqlalchemy-datatables and datatables.js).
+    # Therefore I opened an issue with one of the libraries developers:
+    # https://github.com/Pegase745/sqlalchemy-datatables/issues/35
+
+    # Joins do not work with DataTable
+    # query = session.query(Document, Author).filter(Document.year != None).filter(Document.cite_count != None)\
+    #     .join(Author, Author.id == Document.author_id)
+    query = session.query(Document).filter(Document.year != None).filter(Document.cite_count != None)
+    row_table = DataTables(request.args, Document, query, columns)
+    return jsonify(**row_table.output_result())
 
 
 @app.teardown_request
@@ -309,7 +408,6 @@ def teardown_request(exception):
     session.remove()
 
 # co author networks:
-
 
 @app.route('/co/EntityByID/<i>',  methods=['POST', 'GET'])
 def getEntityById(i):
