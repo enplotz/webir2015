2ce013c246202926584b8c9c9101f419cc53bd86
Philipp94kn <Philipp.Meschenmoser@uni.kn>
2016-01-31 16:15:43 +0100

    update orgfinal & geodesicsfinal

--- webapp/queries/geo_m.py
+++ webapp/queries/geo_m.py
@@ -9,14 +9,14 @@ def getData(i):
         if i % 2 == 0:
             agg += '  AS DECIMAL(10,3))::character varying '
     q = 'SELECT id,val, org.name,  org.lat,org.lng FROM'
-    q += '(SELECT '+agg + ' AS val, org FROM authors WHERE org IS NOT NULL GROUP BY org) a, org '
-    q += ' WHERE a.org=org.id AND org.lng!=48.6618471 AND org.lat!=9.0036649'
+    q += '(SELECT '+agg + ' AS val, org FROM authors WHERE org IS NOT NULL GROUP BY org) a, orgfinal org '
+    q += ' WHERE a.org=org.id AND org.lng IS NOT NULL'
     return q
 
 def getClosest(lat,lon,zoom):
     maxDist = 156543.03392 * math.cos(lat * math.pi / 180) / math.pow(2, zoom)
     maxDist /= 5000
     q = 'WITH click AS (SELECT ST_MakePoint('+str(lon)+','+str(lat)+') AS coord) '
-    q += 'SELECT * FROM org, click WHERE ST_Distance(click.coord, ST_MakePoint(org.lat,org.lng))<'+ str(maxDist)
-    q += ' AND org.lng!=48.6618471 AND org.lat!=9.0036649 ORDER BY ST_Distance(click.coord, ST_MakePoint(org.lng,org.lat)) ASC LIMIT 5'
+    q += 'SELECT * FROM orgfinal org, click WHERE ST_Distance(click.coord, ST_MakePoint(org.lat,org.lng))<'+ str(maxDist)
+    q += ' AND org.lng IS NOT NULL ORDER BY ST_Distance(click.coord, ST_MakePoint(org.lng,org.lat)) ASC LIMIT 5'
     return q
